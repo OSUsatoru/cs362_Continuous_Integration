90dfe60a013b41b5261abe98fc493b391445324d
OSUsatoru <yamamsat@oregonstate.edu>
2021-03-11 10:15:17 -0800

    updata spacing and ','

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ import calculator
 class TestCalculator:
 
     def test_add(self):
-        assert 5 == calculator.add(1,4)
+        assert 5 == calculator.add(1, 4)
 
     def test_subtract(self):
-        assert 2 == calculator.subtract(5,3)
+        assert 2 == calculator.subtract(5, 3)
